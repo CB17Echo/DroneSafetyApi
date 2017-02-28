dde4f6aebacbdb27b0d00fd4224f6f9719152931
rz279 <rz279@cam.ac.uk>
2017-02-28 09:32:47 +0000

    HazardsToHeatmapResponse documentation

--- src/DroneSafetyApi/Services/HazardsToHeatmapResponse.cs
+++ src/DroneSafetyApi/Services/HazardsToHeatmapResponse.cs
@@ -5,10 +5,20 @@ using Microsoft.Azure.Documents.Spatial;
 
 namespace DroneSafetyApi.Services
 {
+    /// <summary>
+    /// The HazardsToHeatmapResponse class is a service to generate <see cref="IHeatmap"/>s and map
+    /// a collection of <see cref="Hazard"/>s onto them
+    /// </summary>
     public class HazardsToHeatmapResponse : HazardsToHeatmapsResponseNoCompositionSendAllSources
     {
+        /// <summary>
+        /// <see cref="HazardsToHeatmapsResponseNoCompositionSendAllSources.HazardsToHeatmapsResponseNoCompositionSendAllSources(IHeatmapFactory)"/>
+        /// </summary>
         public HazardsToHeatmapResponse(IHeatmapFactory heatmapsFactory) : base(heatmapsFactory) { }
 
+        /// <summary>
+        /// <see cref="HazardsToHeatmapsResponseNoCompositionSendAllSources.ConvertToHeatmap(Bounds, int, IEnumerable{Hazard})"/>
+        /// </summary>
         public override IHeatmap ConvertToHeatmap(Bounds area, int numberLonPoints, IEnumerable<Hazard> hazards)
         {
             // Initialise Heatmap
