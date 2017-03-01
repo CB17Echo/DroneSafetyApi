475744bca07aeb50d6ea3b0e7a28799ca2b43135
Joshua Blake <joshbblake@gmail.com>
2017-03-01 10:14:54 +0000

    Cast to int should be last step

--- src/DroneSafetyApi/Models/HeatmapsQueryParsed.cs
+++ src/DroneSafetyApi/Models/HeatmapsQueryParsed.cs
@@ -50,7 +50,7 @@ namespace DroneSafetyApi.Models
             {
                 double deltaLongitude = (CornerOne.Longitude - CornerTwo.Longitude) / 2;
                 double deltaLatitude = (CornerOne.Latitude - CornerTwo.Latitude) / 2;
-                return ((int)Math.Sqrt(deltaLongitude * deltaLongitude + deltaLatitude * deltaLatitude)) * Heatmap.MetresInLatDegree;
+                return (int)(Math.Sqrt(deltaLongitude * deltaLongitude + deltaLatitude * deltaLatitude) * Heatmap.MetresInLatDegree);
             }
         }
     }
